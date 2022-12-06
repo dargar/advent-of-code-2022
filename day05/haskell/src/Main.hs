1fada2bd09dbee7d4dae8e84ce98cc262a4aa0d8
Fredrik Estelius <fredrik.estelius@gmail.com>
2022-12-06 21:04:13 +0100

    day05 haskell refactoring

--- day05/haskell/src/Main.hs
+++ day05/haskell/src/Main.hs
@@ -14,30 +14,28 @@ main = do
   where
     answer movingFunction input =
       let sections = splitOn "\n\n" input in
-      let initialCrates = parseCrates $ sections !! 0 in
-      let instructions = parseInstructions $ sections !! 1 in
-      let movedCrates = foldl movingFunction initialCrates instructions in
+      let crates = parseCrates $ head sections in
+      let instructions = parseInstructions $ head $ tail sections in
+      let movedCrates = foldl movingFunction crates instructions in
       topCrates movedCrates
 
-    parseCrates = map (filter isAlpha) . map snd . filter ((== 0) . flip mod 4 . fst) . zip [0..] . drop 1 . transpose . lines
+    parseCrates = filter (not . null) . map (filter isAlpha) . transpose . lines
     parseInstructions = map (mapMaybe readMaybe) . map words . lines
 
-    moveCratesOneByOne crates [n, from, to] = head $ drop n $ iterate (moveCratesOneByOne' from to) crates
-    moveCratesOneByOne crates _ = undefined
+    moveCratesOneByOne crates instruction = moveCrates reverse crates instruction
+    moveCratesAsStack crates instruction = moveCrates id crates instruction
 
-    moveCratesOneByOne' from to crates =
-      let (as, b:bs) = splitAt (from - 1) crates in
-      let crate = head b in
-      let crates' = as ++ (drop 1 b):bs in
-      let (as, b:bs) = splitAt (to - 1) crates' in
-      as ++ (crate:b):bs
+    moveCrates f crates [n, from, to] =
+      let (cs, crates') = takeFrom (from - 1) n crates in
+      appendTo (to - 1) (f cs) crates'
+    moveCrates _ _ _ = undefined
 
-    moveCratesAsStack crates [n, from, to] =
-      let (as, b:bs) = splitAt (from - 1) crates in
-      let cs = take n b in
-      let crates' = as ++ (drop n b):bs in
-      let (as, b:bs) = splitAt (to - 1) crates' in
-      as ++ (cs ++ b):bs
-    moveCratesAsStack crates _ = undefined
+    takeFrom i n xss =
+      let (removed, remaining) = splitAt n $ xss !! i in
+      (removed, take i xss ++ [remaining] ++ drop (i + 1) xss)
+    appendTo i xs xss =
+      let updated = xs ++ xss !! i in
+      take i xss ++ [updated] ++ drop (i + 1) xss
 
     topCrates = map head . filter (not . null)
+
