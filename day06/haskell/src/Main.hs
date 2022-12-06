b027fa74b220fe97f9a94c27667875f4cdf45035
Fredrik Estelius <fredrik.estelius@gmail.com>
2022-12-06 19:37:18 +0100

    day06 haskell refactoring

--- day06/haskell/src/Main.hs
+++ day06/haskell/src/Main.hs
@@ -6,13 +6,11 @@ main :: IO ()
 main = do
   input <- readFile "../input"
   putStrLn $ "First answer: " ++ (show $ findFirstMarker 4 input)
-  putStrLn $ "First answer: " ++ (show $ findFirstMarker 14 input)
+  putStrLn $ "Second answer: " ++ (show $ findFirstMarker 14 input)
   where
-    findFirstMarker seqLen = fst . head . filter (isMarker . snd) . zip [seqLen..] . windows seqLen
+    findFirstMarker len = (+ len) . head . positionsBy (isMarker len) . windows len
 
     windows n xs = transpose [drop n' xs | n' <- [0..(n-1)]]
-    isMarker signalSequence =
-      let originalLength = length signalSequence in
-      let dedupedLength = length $ nub signalSequence in
-      originalLength == dedupedLength
+    positionsBy p = map fst . filter (p . snd) . zip [0..]
+    isMarker len = (== len) . length . nub
 
