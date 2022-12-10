dc06d175cb6a426a416390297860e5b465727349
Fredrik Estelius <fredrik.estelius@gmail.com>
2022-12-10 13:33:04 +0100

    day10 haskell refactoring

--- day10/haskell/src/Main.hs
+++ day10/haskell/src/Main.hs
@@ -8,7 +8,7 @@ main = do
   putStrLn $ "Second answer:\n" ++ secondAnswer ss
   where
     firstAnswer = show . sum . map (uncurry (*)) . take 6 . everyNth 40 . drop 19 . zip [1..]
-    secondAnswer = unlines . chunksOf 40 . map pixel . zip [0..]
+    secondAnswer = unlines . chunksOf 40 . map pixel . zip screenCoords
 
 states :: String -> [Int]
 states = snd . foldl exec (1, []) . map words . lines
@@ -17,8 +17,11 @@ states = snd . foldl exec (1, []) . map words . lines
     exec (x, ss) ["addx", n] = (x + read n, x:x:ss)
     exec _ _ = undefined
 
+screenCoords :: [Int]
+screenCoords = map (flip mod 40) [0..]
+
 pixel :: (Int, Int) -> Char
-pixel (c, x) = (\screenX -> if x - 1 <= screenX && screenX <= x + 1 then '#' else '.') $ mod c 40
+pixel (screenX, x) = if x - 1 <= screenX && screenX <= x + 1 then '#' else '.'
 
 everyNth :: Int -> [a] -> [a]
 everyNth n = map snd . filter ((== 0) . flip mod n . fst) . zip [0..]
